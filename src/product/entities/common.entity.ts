47b1e5d25454701c7d7318c0aeabcdbbbb1c3d20
yubincho <yubincho9@gmail.com>
2023-07-08 16:50:04 +0900

    product entity, common entity

---
+++ src/product/entities/common.entity.ts
@@ -0,0 +1,12 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export abstract class CommonEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  @UpdateDateColumn()
+  public updatedAt: Date;
+}
--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,15 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CommonEntity } from "./common.entity";
+
+@Entity()
+export class Product extends CommonEntity {
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public desc: string;
+
+  @Column()
+  public price: number;
+}
